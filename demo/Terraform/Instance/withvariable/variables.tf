58ddc5ff1c15af9e5a7a1acfdcad8343546745bd
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-02 22:36:52 +0530

    Update variables.tf

--- demo/Terraform/Instance/withvariable/variables.tf
+++ demo/Terraform/Instance/withvariable/variables.tf
@@ -1,12 +1,16 @@
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "Devserver"
+  default     = "Dev"
 }
 
 variable "ami" {
-        default = "ami-0dfcb1ef8550277af"
+        default = "ami-0b5eea76982371e91"
 }
 variable "instance_type" {
         default = "t2.micro"
+}
+
+variable "sg" {
+        default = "sg-044d9895e1f43efb1"
 }
