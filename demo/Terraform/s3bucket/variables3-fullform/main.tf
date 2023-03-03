d6383a8a1657f05d8cdd7255eae20ab02139fa9d
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-03 20:21:12 +0530

    Update main.tf

--- demo/Terraform/s3bucket/variables3-fullform/main.tf
+++ demo/Terraform/s3bucket/variables3-fullform/main.tf
@@ -1,7 +1,17 @@
 resource "aws_s3_bucket" "my-bucket" {
      bucket = var.bucket_name
      acl    = var.acl 
-     tags = "${var.tags}"
+     tags  = {
+    Ticket = "TASK0001"
+    Purpose = "Backup"
+    Owner = "mitinfraeng@test.com"
+    Name = "datasync_task_name"
+    Group = "mitinfraeng@test.com"
+    Function = "Storage"
+    Environment = "PRD"
+    Deployer = "shree.nayak@test.com"
+    "Customer Facing" = "No"
+    }
      versioning {
        enabled = true
       }
