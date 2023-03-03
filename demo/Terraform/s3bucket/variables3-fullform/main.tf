bba519e1efca24723e1c8996613d01a400b9fa9a
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-03 20:02:42 +0530

    Update main.tf

--- demo/Terraform/s3bucket/variables3-fullform/main.tf
+++ demo/Terraform/s3bucket/variables3-fullform/main.tf
@@ -5,30 +5,4 @@ resource "aws_s3_bucket" "my-ownbucket" {
      versioning {
        enabled = true
       }
-     force_destroy = true #all objects (including any locked objects) should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable.
-
-     website {
-        index_document = "index.html"
-        error_document = "error.html"
-        }
-
-     lifecycle_rule {
-       id      = "log"
-       enabled = true
-       prefix = "log/"
-       tags = {
-       rule      = "log"
-       autoclean = "true"
-      }
-     transition {
-      days          = 30
-      storage_class = "STANDARD_IA" # or "ONEZONE_IA"
-    }
- 
-    logging {
-      target_bucket = aws_s3_bucket.log_bucket.id
-      target_prefix = "log/"
-    }
-  }
-}  
-
+}
