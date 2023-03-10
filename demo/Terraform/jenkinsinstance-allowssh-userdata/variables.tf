890be6e06ba798e49b02ce63506e7499b668915b
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-10 18:09:18 +0530

    Create variables.tf

---
+++ demo/Terraform/jenkinsinstance-allowssh-userdata/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type    = "string"
+  default = "us-east-1"
+}
+variable "ami_id" {
+  type = "map"
+  default = {
+    ap-south-1   = "ami-052cef05d01020f1d"
+    us-east-1   = "ami-0dfcb1ef8550277af"
+  }
+}
+variable "instance_type" {
+  type    = "string"
+  default = "t2.micro"
+}
+variable "key_name" {
+  type    = "string"
+  default = "new-ravi-saro"
+}
