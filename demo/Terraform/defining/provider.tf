535d9d3b15c136f67c6580e929caa73e86843c3a
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-14 16:17:56 +0530

    Update provider.tf

--- demo/Terraform/defining/provider.tf
+++ demo/Terraform/defining/provider.tf
@@ -1,9 +1,30 @@
 provider.tf 
 -------------
 provider "aws" {
+ //aws version 
 //region
 //access key
 //security
 }
+
+--------------------------------------------------
+  
+[ Static method ]
+
+1.syntax: aws version
+version    = "~> 2.0"
+
+
+2.syntax: region 
+region = "us-east-1"
+
+3.syntax: access key & secret key 
+access_key =
+secret_key =
+  
+
+
+
+
 
 
