763cf0c1d826d9691ffacd23cad7293b472a5aa3
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-14 16:09:49 +0530

    Update output.tf

--- demo/Terraform/defining/output.tf
+++ demo/Terraform/defining/output.tf
@@ -1,1 +1,30 @@
+syntax:
 
+output "anyname" {
+  value="${module-name.logical-name.attribute}"
+} 
+
+
+
+Note: module-name : aws_instance
+      logical-name : web-server
+       attribute : instance_state ( these attributes are predefined values )
+
+--------------------------------------------------------------------
+Example:
+
+output "instance_state" {
+  value = "${aws_instance.web-server.instance_state}"
+}
+output "private-ip" {
+  value = "${aws_instance.web-server.private_ip}"
+}
+output "server-pulic_IP" {
+  value = "${aws_instance.web-server.public_ip}"
+}
+output "server-arn" {
+  value = "${aws_instance.web-server.arn}"
+}
+output "server-dns" {
+  value = "${aws_instance.web-server.public_dns}"
+}
