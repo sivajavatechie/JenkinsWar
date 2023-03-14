f06c9465c98a5cc14f1150e807463f680b9a66fa
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-14 16:20:51 +0530

    Update provider.tf

--- demo/Terraform/defining/provider.tf
+++ demo/Terraform/defining/provider.tf
@@ -12,7 +12,7 @@ provider "aws" {
 [ Static method ]
 
 1.syntax: aws version
-version    = "~> 2.0"
+version    = "~> 2.0" 
 
 
 2.syntax: region 
@@ -22,9 +22,18 @@ region = "us-east-1"
 access_key =
 secret_key =
   
+================================================
 
+[ Dynamic method : passing variable ]
+1.syntax: aws version
+Note: usually passed as static only.
 
+2. syntax: region
+region     = "${var.region}"
 
+3.syntax: access key & secret key 
+access_key = "${var.access_key}"
+secret_key = "${var.secret_key}"
 
 
 
