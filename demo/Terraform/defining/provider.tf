b029baf2828f9a55c9b5ac2d9f7c0a61754b80cd
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-14 17:03:56 +0530

    Update provider.tf

--- demo/Terraform/defining/provider.tf
+++ demo/Terraform/defining/provider.tf
@@ -19,8 +19,8 @@ version    = "~> 2.0"
 region = "us-east-1"
 
 3.syntax: access key & secret key 
-access_key =
-secret_key =
+access_key = "AKIAWK3F6TTGC2L42SNE"
+secret_key = "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"
  
 4.aws credential file
 shared_credentials_file = "/root/.aws/credentials"         //server
@@ -37,8 +37,8 @@ Note: usually passed as static only.
 region     = "${var.region}" (or) var.region           // variable "region" {}
 
 3.syntax: access key & secret key 
-access_key = "${var.access_key}"       // variable "access_key" {}
-secret_key = "${var.secret_key}"       // variable "secret_key" {}
+access_key = "${var.access_key}"       // variable "access_key" {}  (or)  variable "access_key" { default = "AKIAWK3F6TTGC2L42SNE" }  #network-module/variables.tf
+secret_key = "${var.secret_key}"       // variable "secret_key" {}  (or)   variable "secret_key" {  default = "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"  }  #network-module/variables.tf
 
 
 
