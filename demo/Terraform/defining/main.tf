99f05a2acaac9bc0c787f985a23e8a7f0da8c2fc
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-14 16:36:39 +0530

    Update main.tf

--- demo/Terraform/defining/main.tf
+++ demo/Terraform/defining/main.tf
@@ -13,19 +13,30 @@ resource "aws_instance" "web-server" {
   //arguments
 }
 
----------------------------------------------
+=====================================================
   
+[ static method ]
 arguments defined inside resource module:
 
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "t2.micro"
   key_name      = "night"
+  
   tags = {
     Name = "Dev-server"
          }
 
 
+=====================================================
 
+[ Dynamic method ]
+
+  ami           = var.ami                           // variable "ami" {}
+  instance_type = var.hardware                      // variable "hardware" {}
+  key_name      = var.keyname                       // variable "keyname" {}
+  tags = {
+    Name = "This server by terraform code"
+  }
 
 
 
