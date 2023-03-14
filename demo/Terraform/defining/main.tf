2eed92136dc9e6a722f29994a5d63c0c83a881ed
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-14 16:24:33 +0530

    Update main.tf

--- demo/Terraform/defining/main.tf
+++ demo/Terraform/defining/main.tf
@@ -1,4 +1,34 @@
-main.tf
-------------------
-resource "modulename" "logical-name" {
+resource module: "syntax"
+
+resource "module-name" "logical-name" {
 }
+
+
+--------------------------------------------
+  
+resource module: "Example"
+
+resource "aws_instance" "web-server" {
+ 
+  //arguments
+}
+
+---------------------------------------------
+  
+arguments defined inside resource module:
+
+  ami           = "ami-026b57f3c383c2eec"
+  instance_type = "t2.micro"
+  key_name      = "night"
+  tags = {
+    Name = "Dev-server"
+         }
+
+
+
+
+
+
+
+
+
