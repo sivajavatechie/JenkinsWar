32796b2cbe7b57c62ab87b3cf28ca2f9d9a9158a
Vimala-Daniel <118675596+Vimala-Daniel@users.noreply.github.com>
2023-03-17 13:18:30 +0530

    Delete demo/Terraform/IAM/user directory

--- demo/Terraform/IAM/user/moreusers/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region     = "us-east-1"
-}
-resource "aws_iam_user" "demo" {
-  count = 3
-  name = "blue.${count.index}"
-}
--- demo/Terraform/IAM/user/singleuser/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region     = "us-east-1"
-}
-resource "aws_iam_user" "demouser" {
-  count = 1
-  name = "april"
-}
